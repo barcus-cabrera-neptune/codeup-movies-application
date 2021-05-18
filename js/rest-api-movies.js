ed92044eb32b05a27a49870d20876dfd1edc7d4f
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-18 15:45:12 -0500

    added loading gif

--- js/rest-api-movies.js
+++ js/rest-api-movies.js
@@ -1,1 +1,20 @@
 "use strict";
+
+
+/*
+    $(window).load(function() {
+        alert("window finished loading");
+    })
+*/
+
+const getMovies = () => {
+    fetch("https://lunar-spice-chocolate.glitch.me/movies")
+        .then(resp => resp.json())
+        .then(movies => {
+            console.log(movies);
+        })
+
+}
+    getMovies();
+
+
