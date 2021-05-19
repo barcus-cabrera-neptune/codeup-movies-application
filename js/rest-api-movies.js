3ab4443ac33ed7ed52bd44bb97757717393b779e
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-19 10:05:11 -0500

    fixed loading issue

--- js/rest-api-movies.js
+++ js/rest-api-movies.js
@@ -7,12 +7,12 @@
 //     console.log('page is fully loaded');
 // });
 
-$('body').append('<div style="" id="loadingDiv"><div class="loader"><img src="../img/Loading.gif" alt="loadingGif"></div></div>');
-$(window).on('load', function(){
+
+/*$(window).on('load', function(){
     setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
-});
+});*/
 function removeLoader(){
-    $( "#loading-image" ).fadeOut(1000, function() {
+    $( "#loadingDiv" ).fadeOut(1000, function() {
         // fadeOut complete. Remove the loading div
         $( "#loadingDiv" ).remove(); //makes page more lightweight
     });
@@ -20,15 +20,20 @@ function removeLoader(){
 
 //Get Method?
 const getMovies = () => {
+    $('#container').append('<div style="" id="loadingDiv"><div class="loader"><img src="img/Loading.gif"' +
+        ' alt="loadingGif"></div></div>');
     fetch("https://lunar-spice-chocolate.glitch.me/movies")
         .then(resp => resp.json())
         .then(movies => {
+            removeLoader();
             console.log(movies);
             let htmlStr = "";
             for (let movie of movies) {
                 htmlStr += `<h1>${movie.title}</h1><p>Director: ${movie.director} <br> ${movie.actors}</p>`;
             }
-            $("#container").html(htmlStr);
+            setTimeout(function (){
+                $("#container").html(htmlStr);
+            }, 1050)
 
         });
 }
