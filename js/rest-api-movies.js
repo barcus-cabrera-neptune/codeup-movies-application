5de399093616635bdc973d90d8214d4c0180d3ab
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-05-19 12:45:59 -0500

    added the ability to add movies

--- js/rest-api-movies.js
+++ js/rest-api-movies.js
@@ -29,8 +29,9 @@ const getMovies = () => {
             console.log(movies);
             let htmlStr = "";
             for (let movie of movies) {
-                htmlStr += `<h1>${movie.title}</h1><p>Director: ${movie.director} <br> ${movie.actors}</p>`;
+                htmlStr += `<h1>${movie.title}</h1><p><strong>Director:</strong> ${movie.director} <br><strong>Actors: </strong> ${movie.actors}</p>`;
             }
+
             setTimeout(function (){
                 $("#container").html(htmlStr);
             }, 1050)
@@ -41,3 +42,39 @@ const getMovies = () => {
 
 
 
+
+    $('#newMovie').click(() =>{
+
+        let addMovie = {
+            title : $('#titleName').val(),
+            director : $('#directorName').val(),
+            rating : $('#ratingValue').val()
+        }
+
+        let postOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(addMovie)
+        }
+
+        // console.log(addMovie.title);
+        fetch("https://lunar-spice-chocolate.glitch.me/movies",postOptions)
+            .then(resp => resp.json())
+            .then(getMovies);
+
+       // fetch("https://lunar-spice-chocolate.glitch.me/movies")
+       //     .then(resp => resp.json())
+       //     .then(movies => {
+       //         for (let movie of movies) {
+       //             if(movie.title !== addMovie.title || movie.director !== addMovie.director || movie.rating !== addMovie.rating){
+       //                 fetch("https://lunar-spice-chocolate.glitch.me/movies", postOptions)
+       //                     .then(getMovies);
+       //             }else {
+       //                 alert("Hey, that book already exists!");
+       //                 break;
+       //             }
+       //         }
+       //     })
+    });
