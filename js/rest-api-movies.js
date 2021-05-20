5c0027adb9d6f30f8a91d76925324ffb73ae5a5c
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-20 15:42:11 -0500

    working on regaining functionality after refactoring to have form dynamically created in js

--- js/rest-api-movies.js
+++ js/rest-api-movies.js
@@ -32,25 +32,29 @@ const getMovies = () => {
             let htmlStr = "";
             for (let movie of movies) {
                 htmlStr += `<h1>${movie.title}</h1><p><strong>Director:</strong> ${movie.director} <br>`;
-                htmlStr += `<strong>Actors: </strong> ${movie.actors}<br><strong>Rating: </strong> ${movie.rating}<br><strong>Id : </strong>${movie.id}</p><button class="editMovie" >Edit Movie Info</button>`;
-                htmlStr += `<button class="deleteMovie" type="button">Delete</button>`;
+                htmlStr += `<strong>Actors: </strong> ${movie.actors}<br><strong>Rating: </strong> ${movie.rating}<br><strong>Id : </strong>${movie.id}</p>`;
+                htmlStr += `<label for="editName"></label>`;
+                htmlStr += `<input type="text" class="${movie.id}" placeholder="Title">`;
+                htmlStr += `<label for="editDirector"></label>`;
+                htmlStr += `<input type="text" class="${movie.id}" placeholder="Director(s)">`;
+                htmlStr += `<label for="editRating"></label>`;
+                htmlStr += `<input type="text" class="editRating[${movie.id}" placeholder="Rating">`;
+                htmlStr += `<button class="editMovie" type="button" data-id="${movie.id}">Edit Movie Info</button>`;
+                htmlStr += `<button class="deleteMovie" type="button" data-id="${movie.id}">Delete</button>`;
 
 
             }
-            // for(let i = 0; i < movies.title.length; i++){
-            //     var selctorOpt =
-            // }
-
 
             setTimeout(function (){
-                $("#container").html(htmlStr);
-                $('.editMovie').click(() =>{
-                    console.log("hello");
+                $("#container").html(htmlStr)
+                $('.editMovie').click((e) =>{
+                    let movieID = $(e.target).data("id");
+                    console.log($(this));
 
                     let editMovie = {
-                        title: $('#editName').val(),
-                        director: $('#editDirector').val(),
-                        rating: $('#editRating').val()
+                        title: $('.editName').val(),
+                        director: $('.editDirector').val(),
+                        rating: $('.editRating').val()
                     }
 
                     let putOptions = {
@@ -61,10 +65,27 @@ const getMovies = () => {
                         body: JSON.stringify(editMovie)
                     }
 
-                    fetch(`https://lunar-spice-chocolate.glitch.me/movies/2`,putOptions)
+
+                    fetch(`https://lunar-spice-chocolate.glitch.me/movies/${movieID}`, putOptions)
+
                         .then(resp => resp.json())
                         .then(getMovies);
-                });
+                })
+
+                $('.deleteMovie').click(function (e){
+                    var deleteMovie = $(e.target).data("id");
+                    let deleteOptions = {
+                        method: 'DELETE',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        }
+                    };
+                    fetch(`https://lunar-spice-chocolate.glitch.me/movies/${deleteMovie}`, deleteOptions)
+
+                        .then (resp => resp.json())
+                        .then(getMovies);
+                })
+
             }, 1050)
 
         });
